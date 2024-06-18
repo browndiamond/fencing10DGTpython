24fec6777c58763b6f74ad8e55dbfa307ed4537f
browndiamond <168794696+browndiamond@users.noreply.github.com>
2024-06-19 09:21:59 +1200

    Update Fencing price.py
    added comments Mr lier

--- Fencing price.py
+++ Fencing price.py
@@ -3,7 +3,7 @@
 # Date 
 # Version 1
 
-def number_check(questions):
+def number_check(questions): # defines number check                                                                                                                                                                                                                                                                                      
     eror = "put in a number bigger than zero/n"
     while True: 
         try:
@@ -14,16 +14,16 @@ def number_check(questions):
                 print(eror)
         except ValueError:
             print (eror)
-keepgoing="Y"
+keepgoing="Y" # keep on cycle 
 while keepgoing =="Y":  
-    price= number_check("price per meter ")
+    price= number_check("price per meter ") # uses number check 
     width = number_check ("width")
     hight= number_check ("hight")
     perimeter= (width+hight)*2
     cost=(perimeter)*price 
-    print(f"Fencing cost per meter: = {cost} ")
+    print(f"Fencing cost per meter: = {cost} ") # shows fencing cost 
     keepgoing = input ("Continue? Y/N ")
-    if keepgoing!=("N"):
+    if keepgoing!=("N"): # end cycle 
         keepgoing="Y"
 print("Thanks for using this program")
 
